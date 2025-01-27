0ebb2bd2cc68a65a88927b799648479e6eb07b0b
Yvesco75 <yvesgodonou4@gmail.com>
2025-01-27 19:25:20 +0100

    Fonction lancé de dé en boucle

--- exo2.dart
+++ exo2.dart
@@ -20,13 +20,14 @@ int lancerDe() {
 
 import 'dart:math';
 
-/// Fonction principale
+// Fonction principale
 void main() {
   int nombreDeLancers = 10; // Définir le nombre de lancers
   lancerDeMultiple(nombreDeLancers);
 }
 
-/// Fonction qui effectue plusieurs lancers de dé
+// Fonction qui effectue plusieurs lancers de dé
+
 void lancerDeMultiple(int nombreDeLancers) {
   print("Lancer $nombreDeLancers fois le dé :");
 
@@ -36,7 +37,46 @@ void lancerDeMultiple(int nombreDeLancers) {
   }
 }
 
+// Fonction qui génère un nombre aléatoire entre 1 et 6
+int lancerDe() {
+  return Random().nextInt(6) + 1; // Génère un nombre aléatoire entre 1 et 6
+}
+
+
+///Exercice dé en boucle
+
+import 'dart:math';
+
+/// Fonction principale
+void main() {
+  int objectif = 3;
+  lancerDeEnBoucle(objectif);
+}
+
+/// Fonction qui effectue plusieurs lancers de dé
+
+void lancerDeEnBoucle(int objectif) {
+  print("lancer le dé jusqu'a obtenir $objectif fois 6");
+  int compteur = 0;
+  int lancers = 0;
+
+  //Boucle à exécuter tant l'objectif n'est pas atteint
+  while (compteur < objectif) {
+    lancers++;
+    int resultat = lancerDe();
+    print("Lancer $lancers : $resultat");
+    if (resultat == 6) {
+      compteur++;
+    }
+  }
+}
+
 /// Fonction qui génère un nombre aléatoire entre 1 et 6
 int lancerDe() {
   return Random().nextInt(6) + 1; // Génère un nombre aléatoire entre 1 et 6
 }
+
+
+
+
+
