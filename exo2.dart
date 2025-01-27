e8ed4ed314bb94d7aa4518d3c1d15bd6be785588
Yvesco75 <yvesgodonou4@gmail.com>
2025-01-27 15:58:15 +0100

    Ajout de la fonction lancerDeMultiple

--- exo2.dart
+++ exo2.dart
@@ -1,4 +1,5 @@
 
+// lancer de simple
 
 import 'dart:math';
 
@@ -10,4 +11,32 @@ void main() {
 int lancerDe() {
   Random random = Random();
   return random.nextInt(6) + 1; 
+}
+
+
+
+
+/// Fonction qui effectue plusieurs lancers de dé et affiche le résultat
+
+import 'dart:math';
+
+/// Fonction principale
+void main() {
+  int nombreDeLancers = 10; // Définir le nombre de lancers
+  lancerDeMultiple(nombreDeLancers);
+}
+
+/// Fonction qui effectue plusieurs lancers de dé
+void lancerDeMultiple(int nombreDeLancers) {
+  print("Lancer $nombreDeLancers fois le dé :");
+
+  for (int i = 1; i <= nombreDeLancers; i++) {
+    int resultat = lancerDe();
+    print("Lancer $i : $resultat");
+  }
+}
+
+/// Fonction qui génère un nombre aléatoire entre 1 et 6
+int lancerDe() {
+  return Random().nextInt(6) + 1; // Génère un nombre aléatoire entre 1 et 6
 }
